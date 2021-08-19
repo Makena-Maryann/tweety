bc56ffcf8d11b00b984c46d6e011afa2dc36e2f7
Makena-Maryann <maryann.makena00@gmail.com>
2021-08-19 12:31:36 +0300

    Design the timeline

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,9 +23,11 @@
 
 <body>
     <div id="app">
-        <section class="px-8">
+        <section class="px-8 py-4 mb-6">
             <header class="container mx-auto">
-                <h1>Tweety</h1>
+                <h1>
+                    <img src="/images/logo.svg" alt="Tweety">
+                </h1>
             </header>
         </section>
 
