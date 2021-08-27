34a149aea398d392084d3a4159bd1a81fc194d38
Makena-Maryann <maryann.makena00@gmail.com>
2021-08-27 13:02:07 +0300

    Clean Up

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -8,54 +8,14 @@ use Illuminate\Validation\Rule;
 
 class ProfilesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show(User $user)
     {
-        return view('profiles.show', compact('user'));
+        return view('profiles.show', [
+            'user' => $user,
+            'tweets' => $user->tweets()->paginate(50),
+        ]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit(User $user)
     {
         // $this->authorize('edit', $user);
@@ -63,13 +23,6 @@ class ProfilesController extends Controller
         return view('profiles.edit', compact('user'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(User $user)
     {
         $attributes = request()->validate([
@@ -93,16 +46,5 @@ class ProfilesController extends Controller
         $user->update($attributes);
 
         return redirect($user->path());
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
--- resources/views/_sidebar-links.blade.php
+++ resources/views/_sidebar-links.blade.php
@@ -1,33 +1,29 @@
 <ul>
     <li>
-        <a class="font-bold text-lg mb-4 block" href="{{ route('home') }}"> Home </a>
+        <a class="font-bold text-lg mb-4 block" href="{{ route('home') }}">
+            Home
+        </a>
     </li>
 
     <li>
-        <a class="font-bold text-lg mb-4 block" href="/explore"> Explore </a>
+        <a class="font-bold text-lg mb-4 block" href="/explore">
+            Explore
+        </a>
     </li>
 
-    <li>
-        <a class="font-bold text-lg mb-4 block" href="#">Notifications </a>
-    </li>
+    @auth
+        <li>
+            <a class="font-bold text-lg mb-4 block" href="{{ current_user()->path() }}">
+                Profile
+            </a>
+        </li>
 
-    <li>
-        <a class="font-bold text-lg mb-4 block" href="#"> Messages </a>
-    </li>
+        <li>
+            <form method="POST" action="/logout">
+                @csrf
 
-    <li>
-        <a class="font-bold text-lg mb-4 block" href="#"> Bookmarks </a>
-    </li>
-
-    <li>
-        <a class="font-bold text-lg mb-4 block" href="#"> Lists </a>
-    </li>
-
-    <li>
-        <a class="font-bold text-lg mb-4 block" href="{{ route('profile', auth()->user()) }}"> Profile </a>
-    </li>
-
-    <li>
-        <a class="font-bold text-lg mb-4 block" href="#"> More </a>
-    </li>
+                <button class="font-bold text-lg">Logout</button>
+            </form>
+        </li>
+    @endauth
 </ul>
